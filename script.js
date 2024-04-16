6bf5c0d1f0dbfaffb7afe0a2ea5f0ab5c01ae4db
RAHUL <93506596+rrahul79@users.noreply.github.com>
2024-04-16 18:00:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,41 +2,45 @@ var locationBtn = document.getElementById("loc_btn");
 var AstroBtn = document.getElementById("astro_btn");
 
 locationBtn.addEventListener("click", function () {
+    fetchISSLocation();
+});
+
+AstroBtn.addEventListener("click", function () {
+    fetchAstros();
+});
+
+function fetchISSLocation() {
     fetch("http://api.open-notify.org/iss-now.json")
-        .then((response) => { return response.json(); })
-        .then((data) => {
-            //console.log(data);
+        .then(response => response.json())
+        .then(data => {
             let latitude = data.iss_position.latitude;
             let longitude = data.iss_position.longitude;
 
             document.getElementById("latitude").innerHTML = latitude;
             document.getElementById("longitude").innerHTML = longitude;
-    })
-});
+        })
+        .catch(error => {
+            console.error('Error fetching ISS location:', error);
+        });
+}
 
-AstroBtn.addEventListener("click", function () {
-    let astro_list = [];
+function fetchAstros() {
     fetch("http://api.open-notify.org/astros.json")
-        .then((response) => { return response.json(); })
-        .then((data) => {
-            //let index = 0;
-            for (var i = 0; i < data.people.length ; i++)
-            {
-                if (data.people[i].craft == "ISS")
-                {
-                    astro_list[i] = data.people[i].name;
-                }
-                
-            }
-            let list = document.getElementById("list");
-            for (var i = 0; i < astro_list.length; i++)
-            {
-                let list_item = document.createElement("li");
-                list_item.appendChild(document.createTextNode(astro_list[i]));
-                list.append(list_item);
-            }
-            
-    })
-    
-});
+        .then(response => response.json())
+        .then(data => {
+            let astro_list = data.people
+                .filter(person => person.craft === "ISS")
+                .map(person => person.name);
 
+            let list = document.getElementById("list");
+            list.innerHTML = ""; // Clear previous list
+            astro_list.forEach(name => {
+                let list_item = document.createElement("li");
+                list_item.appendChild(document.createTextNode(name));
+                list.appendChild(list_item);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching astronauts:', error);
+        });
+}
